a3d72b31c575d7593a4575b388d8c7c1a3d4114d
Iuliia <iuliia@lepistina.com>
2017-11-24 11:27:57 -0800

    adding line for too short answer

--- guess.php
+++ guess.php
@@ -8,6 +8,8 @@
 			<?php 
 				if( ! isset($_GET['guess']) ) {
 					echo("Missing guess parameter");
+				}else if( strlen($_GET['guess']) < 1 ) {
+					echo("Your guess is too short");
 				}else if( ! is_numeric($_GET['guess']) ) {
 					echo("Your guess is not a number");
 				} else if($_GET['guess'] < 62){
